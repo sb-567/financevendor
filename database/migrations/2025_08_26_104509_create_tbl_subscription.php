9b42667d4b16065ffca52250f19e551e6e63339b
shubhamcosmic25 <shubhamcosmic25@gmail.com>
2025-08-26 18:50:59 +0530

    migration added subscritin

---
+++ database/migrations/2025_08_26_104509_create_tbl_subscription.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_subscription', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255)->nullable();
+            $table->string('price',255)->nullable();
+            $table->string('cross_price',255)->nullable();
+            $table->string('offer_text',255)->nullable();
+            $table->text('plan_ids')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_subscription');
+    }
+};
