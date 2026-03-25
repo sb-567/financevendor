4c33ee2d2ee396677fa9775dcc7fde5b4b584c04
@shubham8624524 <shubham@millicent.in>
2026-03-25 09:31:36 +0530

    blog fetching

--- app/Http/Controllers/Website/BlogController.php
+++ app/Http/Controllers/Website/BlogController.php
@@ -13,6 +13,12 @@ class BlogController extends Controller
 
      public function getblog(){
          $data['title']='Blog List';
+         $data['blogs']=DB::table('tbl_blogs')
+         ->select('tbl_blogs.*','tbl_blog_categories.title as category')
+         ->Leftjoin('tbl_blog_categories','tbl_blogs.category_id','=','tbl_blog_categories.id')
+         ->where('tbl_blogs.status',1)
+         ->orderBy('tbl_blogs.id','desc')->get();
+
         return view('website.blog',$data);
     }
     public function getblogdetail(){
--- resources/views/website/blog.blade.php
+++ resources/views/website/blog.blade.php
@@ -18,32 +18,19 @@
        
         <div class="container mt-90">
           <div class="row">
-            <div class="col-lg-4 col-sm-12 pr-30 mb-50">
-              <div class="grid-4-img color-bg-9"><a href="{{ route('blogdetail') }}"><img src="{{  asset('wassets/assets/imgs/page/homepage1/img-news-1.png')}}" alt="Agon"></a></div>
-              <div class="card-grid-style-4 mt-3">
-                <span class="tag-dot mb-2">Company</span>
-                <a class="text-heading-5" href="{{ route('blogdetail') }}">We can blend colors multiple ways, the most common</a>
-              </div>
-            </div>
 
-
-            <div class="col-lg-4 col-sm-12 pr-30 mb-50">
-              <div class="grid-4-img color-bg-9"><a href="{{ route('blogdetail') }}"><img src="{{  asset('wassets/assets/imgs/page/homepage1/img-news-1.png')}}" alt="Agon"></a></div>
-              <div class="card-grid-style-4 mt-3">
-                <span class="tag-dot mb-2">Company</span>
-                <a class="text-heading-5" href="{{ route('blogdetail') }}">We can blend colors multiple ways, the most common</a>
-              </div>
-            </div>
-
-
-            <div class="col-lg-4 col-sm-12 pr-30 mb-50">
-              <div class="grid-4-img color-bg-9"><a href="{{ route('blogdetail') }}"><img src="{{  asset('wassets/assets/imgs/page/homepage1/img-news-1.png')}}" alt="Agon"></a></div>
-              <div class="card-grid-style-4 mt-3">
-                <span class="tag-dot mb-2">Company</span>
-                <a class="text-heading-5" href="{{ route('blogdetail') }}">We can blend colors multiple ways, the most common</a>
-              </div>
-            </div>
-
+            @if(!empty($blogs))
+              @foreach ($blogs as $item)
+                  
+                <div class="col-lg-4 col-sm-12 pr-30 mb-50">
+                  <div class="grid-4-img color-bg-9"><a href="{{ route('blogdetail', ['slug' => $item->slug]) }}"><img src="{{  asset('public/uploads/blog/'.$item->image) }}" alt="Agon"></a></div>
+                  <div class="card-grid-style-4 mt-3">
+                    <span class="tag-dot mb-2">{{ $item->category }}</span>
+                    <a class="text-heading-5" href="{{ route('blogdetail', ['slug' => $item->slug]) }}">{{ $item->blog_title }}</a>
+                  </div>
+                </div>
+              @endforeach
+            @endif
 
            
           </div>
