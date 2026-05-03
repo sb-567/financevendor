08a3d87cc09b399805a6c338c574a2af19360540
@shubham8624524 <shubham@millicent.in>
2026-05-03 12:56:55 +0530

    property backend added poperty name and migration added

---
+++ database/migrations/2026_05_03_065216_add_column_to_tbl_leads.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tbl_leads', function (Blueprint $table) {
+            $table->string('property_id')->nullable()->after('lead_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tbl_leads', function (Blueprint $table) {
+            //
+        });
+    }
+};
